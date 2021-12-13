3ab56e670081037b3f78bb38d7c0504b03891298
Babalola Muhammad <barmilitary@yahoo.com>
2021-12-13 09:57:15 +0100

    getting started

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Title } from './components';
+
+const App = () => {
+    return (
+        <div className='App'>
+            <Title />
+        </div>
+    )
+}
+
+export default App
